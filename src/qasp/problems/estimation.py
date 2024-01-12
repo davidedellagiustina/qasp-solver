76c957bbf643140b526a57f46c5f8ebb2ce6feee
davidedellagiustina <davide@dellagiustina.com>
2024-01-12 11:30:04 +0000

    Boilerplate for amplitude estimation

--- src/qasp/problems/estimation.py
+++ src/qasp/problems/estimation.py
@@ -1,4 +1,25 @@
 '''Amplitude estimation algorithms.
 '''
 
-# TODO
+from qiskit import QuantumCircuit
+from ..oracle import QuantumOracle
+
+
+# +--------------------+
+# | Estimation circuit |
+# +--------------------+
+
+def circuit(algorithm: QuantumCircuit, oracle: QuantumOracle, t: int) -> QuantumCircuit:
+    '''Build a quantum circuit implementing the amplitude estimation algorithm.
+
+    #### Arguments
+        algorithm (QuantumCircuit): Ciruit that implements the initialization algorithm.
+        oracle (QuantumOracle): Citcuit that implements the oracle.
+        t (int): Number of decimal (binary) digits to estimate.
+
+    #### Return
+        QuantumCircuit: Built circuit.
+    '''
+    assert t > 0
+    assert algorithm.num_qubits == oracle.num_qubits
+    # TODO
