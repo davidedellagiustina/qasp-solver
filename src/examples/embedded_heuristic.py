291f2e04b60050734151fc7f0d5bb681f2df5ef6
davidedellagiustina <davide@dellagiustina.com>
2024-02-10 17:14:02 +0000

    Added example with Rot used to FIND stable models

---
+++ src/examples/embedded_heuristic.py
@@ -0,0 +1,72 @@
+'''TODO
+'''
+# TODO: Docstring.
+
+from src import qasp
+from src.examples.util import tab, pause
+
+# ASP program
+PRGM = '''
+    p :- not q.
+    q :- not p.
+    r :- p.
+    r :- q.
+'''
+
+# Hardcoded list of stable models
+STABLE_MODELS = [
+    {('p', True), ('q', False), ('r', True)},  # {p, r}
+    {('p', False), ('q', True), ('r', True)},  # {q, r}
+]
+
+# Hardcoded navigation route and relative weight function
+DELTA = [('p', True)]
+WEIGHTS = [1, 1/2, 1/2]  # Order: p < q < r
+
+
+def main():
+    '''Entrypoint.
+    '''
+    print(f'ASP program:\n{tab(PRGM.strip(), striplines=True)}\n')
+    pause()
+
+    # Program parameters
+    n = len(STABLE_MODELS[0])
+    # pylint: disable=invalid-name
+    M = len(STABLE_MODELS)
+    print(f'Number of variables: {n}.')
+    print(f'Number of stable models: {M}.')
+    print()
+    pause()
+
+    # Route
+    route = ', '.join(
+        [('' if value else 'not ') + f'{atom}' for (atom, value) in DELTA]
+    )
+    print(f'Route delta: <{route}>')
+    print()
+    pause()
+
+    # Initialization algorithm
+    algorithm = qasp.init_algorithm.alg_from_weights(WEIGHTS)  # Rot gate
+    print(f'Initialization algorithm:\n{tab(str(algorithm.draw()))}\n')
+    pause()
+
+    # Oracle
+    oracle = qasp.oracle.from_asp_stable_models(
+        STABLE_MODELS, var_order=['p', 'q', 'r'])
+    print(f'Quantum oracle:\n{tab(str(oracle[1].draw()))}\n')
+    pause()
+
+    # Simulation
+    (circuit, iters, stable_model) = qasp.problems.amplification.exec_find_one_known_m(
+        algorithm, oracle, M)
+    print(f'Used circuit:\n{tab(str(circuit.draw()))}\n')
+    pause()
+    print(f'Found stable model: {stable_model}.')
+    print(f'Number of iterations: {iters}.')
+    print()
+
+
+if __name__ == '__main__':
+    main()
